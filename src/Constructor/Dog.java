5a530fa6c66cc3baedc9075d2ea14aa107dcd1ab
Ayush kr Singh <ayushsingh4167@gmail.com>
2025-11-04 23:20:43 +0530

    constructor problem 2

--- src/Constructor/Dog.java
+++ src/Constructor/Dog.java
@@ -5,5 +5,26 @@ package Constructor;
 //as parameters and initializes the instance
 //variables. Print the values of the variables.
 public class Dog {
+    private String name;
+    private String color;
 
+    //constructor taking name and color
+    public Dog(String name,String color){
+        this.name=name;
+        this.color=color;
+    }
+    //getter for name
+    public String getName(){
+        return name;
+    }
+    //getter for color
+    public String getColor(){
+        return color;
+    }
+
+    public static void main(String[] args){
+        Dog dog=new Dog("Tommy","Black");
+        System.out.println("Dog's name :" +dog.name);
+        System.out.println("Dog's color :" +dog.color);
+    }
 }
