be1d13bafdbe7b8833f678894a5fa7665736889f
Ayush kr Singh <ayushsingh4167@gmail.com>
2025-11-04 23:12:17 +0530

    constructor problem 2

---
+++ src/Constructor/Dog.java
@@ -0,0 +1,9 @@
+package Constructor;
+//Write a Java program to create a class called Dog with
+//        instance variables name and color. Implement a parameterized constructor
+//that takes name and color
+//as parameters and initializes the instance
+//variables. Print the values of the variables.
+public class Dog {
+
+}
